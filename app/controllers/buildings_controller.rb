5d808e3811bbf38e200ef8349483d4721a064060
Julio <julio.chazari@gmail.com>
2020-06-29 22:10:59 -0500

    -

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,28 @@
 class BuildingsController < ApplicationController
+    before_action :find_building, only: [:show, :edit, :update]
+
+    def index
+        @buildings = Building.all
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @building.update(building_params)
+        redirect_to @building
+    end
+
+    private
+
+    def building_params
+        params.require(:building).permit(:name, :address, :country, :rent_per_floor)
+    end
+
+    def find_building
+        @building = Building.find(params[:id])
+    end
 end
--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,8 @@
 class Building < ApplicationRecord
+  has_many :offices
+  has_many :companies, through: :offices
+
+  validates :name, :address, :country, :rent_per_floor, presence: true
 
   def number_of_floors_available
     # Will not work until relationships and schema are corretly setup
@@ -13,5 +17,18 @@ class Building < ApplicationRecord
   def empty_offices
     number_of_floors_available.map { |f| offices.build(floor: f) }
   end
+
+  def total_rent
+    # total_offices = []
+    # self.offices.each do |office|
+    #   total_offices << office.company.name
+    # end
+    # self.rent_per_floor * total_offices.count
+    self.offices.count * self.rent_per_floor
+  end
+
+  # def available_floors
+  #   self.number_of_floors -
+  # end
 
 end
